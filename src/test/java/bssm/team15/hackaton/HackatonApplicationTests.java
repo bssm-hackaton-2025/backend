c7e4ee0a02a7cbebf20b526ba15eeae14a1e3a63
leegunwoo <leegunwoo0325@gmail.com>
2025-12-29 20:32:04 +0900

    initial commit

---
+++ src/test/java/bssm/team15/hackaton/HackatonApplicationTests.java
@@ -0,0 +1,13 @@
+package bssm.team15.hackaton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackatonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
